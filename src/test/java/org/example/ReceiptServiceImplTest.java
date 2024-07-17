2a45d7c04d33a80ccf851d212e9837501cf66513
Ivan_Andreev <iandreev@ru.estee.com>
2024-07-17 18:01:03 +0300

    Changes in receipt service test

--- src/test/java/org/example/ReceiptServiceImplTest.java
+++ src/test/java/org/example/ReceiptServiceImplTest.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import org.example.exception.ProductValidationException;
+import org.example.input.Input;
+import org.example.input.InputItem;
 import org.example.order.Order;
 import org.example.order.OrderItem;
 import org.example.order.OrderProduct;
 import org.example.product.ProductType;
+import org.example.service.OrderManagementService;
+import org.example.service.ProductManagementService;
 import org.example.service.ReceiptService;
+import org.example.service.implementation.OrderManagementServiceImpl;
+import org.example.service.implementation.ProductManagementServiceImpl;
 import org.example.service.implementation.ReceiptServiceImpl;
 import org.junit.jupiter.api.Test;
 
@@ -12,21 +19,22 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ReceiptServiceImplTest {
 
     private final ReceiptService receiptService = new ReceiptServiceImpl();
+    private final ProductManagementService productManagementService = new ProductManagementServiceImpl();
+    private final OrderManagementService orderManagementService = new OrderManagementServiceImpl(productManagementService);
 
     @Test
     void testGenerateReceipt() {
         // Given
-        OrderProduct product1 = new OrderProduct("Coffee", new BigDecimal("2.55"), "small", ProductType.BEVERAGE);
-        OrderProduct product2 = new OrderProduct("Bacon Roll", new BigDecimal("4.53"), null, ProductType.SNACK);
-        OrderProduct extraProduct = new OrderProduct("Extra milk", new BigDecimal("0.32"), null, ProductType.EXTRA);
-        OrderItem orderItem1 = new OrderItem(product1, List.of(extraProduct));
-        OrderItem orderItem2 = new OrderItem(product2, List.of());
+        Input input = new Input(List.of(new InputItem("Coffee", "small", List.of("Extra milk")),
+                new InputItem("Bacon Roll", null, List.of())
+                ), 3);
 
-        Order order = new Order(List.of(orderItem1, orderItem2), 3);
+        Order order = orderManagementService.processInput(input);
 
         // When
         String receipt = receiptService.generateReceipt(order);
@@ -36,12 +44,12 @@ class ReceiptServiceImplTest {
                 "Receipt:\n" +
                         "----------------------------\n" +
                         "Coffee (small): $2.55\n" +
-                        " --Extra milk: $0.32\n" +
+                        " --Extra milk: $0\n" +
                         "Bacon Roll: $4.53\n" +
                         "----------------------------\n" +
-                        "Total: $7.40" +
+                        "Total: $7.08" +
                         "----------------------------\n" +
-                        "Current client stamps amount: 3\n";
+                        "Current client stamps amount: 4\n";
 
         assertEquals(expectedReceipt, receipt);
     }
@@ -49,12 +57,11 @@ class ReceiptServiceImplTest {
     @Test
     void testGenerateReceiptWithNoExtras() {
         // Given
-        OrderProduct product1 = new OrderProduct("Coffee", new BigDecimal("2.55"), "small", ProductType.BEVERAGE);
-        OrderProduct product2 = new OrderProduct("Bacon Roll", new BigDecimal("4.53"), null, ProductType.SNACK);
-        OrderItem orderItem1 = new OrderItem(product1, List.of());
-        OrderItem orderItem2 = new OrderItem(product2, List.of());
+        Input input = new Input(List.of(new InputItem("Coffee", "small", List.of()),
+                new InputItem("Bacon Roll", null, List.of())
+        ), 2);
 
-        Order order = new Order(List.of(orderItem1, orderItem2), 2);
+        Order order = orderManagementService.processInput(input);
 
         // When
         String receipt = receiptService.generateReceipt(order);
@@ -68,34 +75,9 @@ class ReceiptServiceImplTest {
                         "----------------------------\n" +
                         "Total: $7.08" +
                         "----------------------------\n" +
-                        "Current client stamps amount: 2\n";
+                        "Current client stamps amount: 3\n";
 
         assertEquals(expectedReceipt, receipt);
     }
 
-    @Test
-    void testGenerateReceiptWithOnlyExtras() {
-        // Given
-        OrderProduct extraProduct1 = new OrderProduct("Extra milk", new BigDecimal("0.32"), null, ProductType.EXTRA);
-        OrderProduct extraProduct2 = new OrderProduct("Foamed milk", new BigDecimal("0.51"), null, ProductType.EXTRA);
-        OrderItem orderItem = new OrderItem(extraProduct1, List.of(extraProduct2));
-
-        Order order = new Order(List.of(orderItem), 1);
-
-        // When
-        String receipt = receiptService.generateReceipt(order);
-
-        // Then
-        String expectedReceipt =
-                "Receipt:\n" +
-                        "----------------------------\n" +
-                        "Extra milk: $0.32\n" +
-                        " --Foamed milk: $0.51\n" +
-                        "----------------------------\n" +
-                        "Total: $0.83" +
-                        "----------------------------\n" +
-                        "Current client stamps amount: 1\n";
-
-        assertEquals(expectedReceipt, receipt);
-    }
 }
